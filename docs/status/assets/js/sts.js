9fec00fb4daf234598b9aa81b894e088dc9e8373
B7X0 <142055331+B7X0@users.noreply.github.com>
2023-08-15 16:39:18 -0300

    Add files via upload

---
+++ docs/status/assets/js/sts.js
@@ -0,0 +1,24 @@
+const st = document.getElementById('st');
+const dt = document.getElementById('dt');
+fetch('https://api.weao.dev/status')
+  .then(response => response.json())
+  .then(bspl => {
+    let item = bspl["Data"]["WeAreDevs API"]["platform"]["Windows"];
+    const milliseconds = item["upd_unix"] * 1000
+    const dateObject = new Date(milliseconds)
+    const humanDateFormat = dateObject.toLocaleString()
+    dt.innerHTML = "Last update: " + dateObject.toLocaleString("en-US", {timeZoneName: "short"});
+      if ((item["status"]) === 0) {
+        console.log('✅ Updated');
+        st.innerHTML = "Working";
+        st.className += 'text-success';
+      } else {
+        console.log('⛔️ Patched');
+        st.innerHTML = "Patched";
+        st.className += 'text-danger';
+      }
+});
+
+
+
+
